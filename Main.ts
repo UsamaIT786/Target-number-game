f47d0ebcc4df7a3d6dde49b5a0b1abbc15aaaca7
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-05-03 08:16:41 -0700

    Add files via upload

---
+++ Main.ts
@@ -0,0 +1,11 @@
+// Target Number Game//
+
+let guess: number = 9;
+let target: number = 6;
+ if (guess>target){
+    console.log("Your Guess is too low")
+ }else if(guess>target){
+    console.log("Your guess is too high")
+ }else{
+    console.log("Your select Correctly")
+ }
---
+++ main.js
@@ -0,0 +1,12 @@
+// Target Number Game//
+var guess = 9;
+var target = 6;
+if (guess > target) {
+    console.log("Your Guess is too low");
+}
+else if (guess > target) {
+    console.log("Your guess is too high");
+}
+else {
+    console.log("Your select Correctly");
+}
